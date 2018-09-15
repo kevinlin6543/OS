13dc7358cbc0e1f6fa17026bfe58b8a00021290c
Kevin Lin <kevin.lin6543@gmail.com>
2018-09-15 19:45:00 -0400

    Final with Error Checking

--- OS_Pset1/OS_PSET1.c
+++ OS_Pset1/OS_PSET1.c
@@ -1,80 +1,82 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 
-	int op;
-	int opterr = 0;
-	int bufsize = 4096;
+	int i, j, op, lim, arg, temp, fdinput;
 	char *buf;
-	int i;
-	int j;
-	int lim;
+	int bufsize = 4096;
 	int start = 1;
-	int arg;
-	int fd = 1;
-	int temp;
+	int fdoutput = 1;
 	int done = 0;
-
-int concat(int fd, int fp)
-	{
-		i = 1;
-		while(i != 0)
-		{
-			//lim = sizeof(buf);
-			i = read(fp,buf,bufsize);
-			j = write(fd, buf, i);
-			//temp = sizeof(buf)/sizeof(buf[0]);
-			printf("%d\n", i);
-		}
-		return 0;
-	}
-
-
+	int opterr = 0;
 
 int main(int argc, char **argv)
 {
-	// Checks the ops and parameters
 	while ((op = getopt(argc, argv, "b:o:?")) != -1 && !done)
 	{
-		switch (op) {
+		switch (op)
+		{
 			case 'b':
 				start += 2;
 				bufsize = atoi(optarg);
+				if (bufsize <= 0)
+				{
+					fprintf(stderr, "Invalid buffer size, %d, chosen. Must be greater than 0", bufsize);
+					return -1;
+				}
 				break;
 			case 'o':
 				start += 2;
-				fd = open(optarg, O_CREAT | O_TRUNC | O_APPEND | O_WRONLY, 0666);
+				fdoutput = open(optarg, O_CREAT | O_TRUNC | O_APPEND | O_WRONLY, 0666);
+				if (fdoutput < 0)
+				{
+					fprintf(stderr, "Unable to Create or open file name: %s. %s\n", optarg, strerror(errno));
+					return -1;
+				}
 				break;
 			default:
 				done = 1;
 				break;
 		}
-		/*
-		if (op == 'b')
-		{
-			bufsize = atoi(optarg);
-			//printf("%d\n", bufsize);
-			start += 2;
-			//bstatus = 1;
-		}
-		if (op == 'o')
-		{
-			//printf("%s\n", optarg);
-			start += 2;
-			fd = open(optarg, O_CREAT | O_TRUNC | O_APPEND | O_WRONLY, 0666);
-		}
-		*/
 	}
+
 	buf = (char *)malloc(sizeof(char) * bufsize);
+
 	for (arg = start; arg < argc; ++arg)
 	{
+		i = 1;
 		if (argv[arg][0] == '-')
 			argv[arg]++;
-		//printf("Arg %d is %s\n", arg, argv[arg]);
-		// inputs[filenum] = argv[arg];
-		i = concat(fd, open(argv[arg], O_RDONLY, 0666));
+
+		fdinput = open(argv[arg], O_RDONLY, 0666);
+
+		if (fdinput < 0)
+		{
+			fprintf(stderr, "Unable to open the file, %s, for reading. %s\n", argv[arg], strerror(errno));
+			return -1;
+		}
+		else
+		{
+			while(i != 0)
+			{
+				i = read(fdinput, buf, bufsize);
+				if (i < 0)
+				{
+					fprintf(stderr, "Unable to read file. File Descriptor: %d. File name: %s. %s\n", fdinput, argv[arg], strerror(errno));
+					return -1;
+				}
+				j = write(fdoutput, buf, i);
+				if (j < 0)
+				{
+					fprintf(stderr, "Unable to write to file. File Descriptor: %d. %s\n", fdoutput, strerror(errno));
+					return -1;
+				}
+			}
+			close(fdinput);
+		}
 	}
 	return 0;
-
 }
