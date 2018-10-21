80ea262817f75faf18fd31a1b783ab3137e88345
Kevin Lin <kevin.lin6543@gmail.com>
2018-10-21 17:08:01 -0400

    added script interpreter

--- OS_Pset3/OS_Pset3.c
+++ OS_Pset3/OS_Pset3.c
@@ -14,41 +14,126 @@
 #include <sys/resource.h>
 
 char *buf, *token, *file, *iored;
-char execpath[PATH_MAX];
-//char *token;
+char execpath[PATH_MAX], c;
+char *scriptpath;
 char cwd[PATH_MAX];
 char *argv[100];
 size_t bufsize = 4096;
 ssize_t i;
-int j, k, n, fd, pid;
-//char *file;
-//char *iored;
+int j, k, n, fd, fds, pid, script;
 struct rusage usage;
 double usertime, systime;
+
+int scriptread(int filed, char *buffer, int s)
+{
+  k = 0;
+  while ( (i = read(filed, &c, 1)) != 0)
+  {
+    //fprintf(stderr, "read = %ld;chara = %c\n",i, c);
+    if (c == '\0')
+    {
+      s = 0;
+      break;
+    }
+    if (c == '\n')
+    {
+      buffer[k] = '\0';
+      //printf("command = %s\n", buffer);
+      break;
+    }
+    buffer[k] = c;
+    k++;
+  }
+  return i;
+}
 
 int main()
 {
   buf = (char *)malloc(bufsize * sizeof(char));
   while(1)
   {
-    printf("$ ");
+    //fprintf(stderr, "Loop start\n" );
     j = 0;
-    if((i = getline(&buf, &bufsize, stdin)) > 0)
+    if (!script)
+    {
+      printf("$ ");
+      i = getline(&buf, &bufsize, stdin);
+    }
+    if(script || i > 0)
     {
       if(buf[0] == '#')
         continue;
-      buf[i-1] = '\0';
+      if (!script)
+        buf[i-1] = '\0';
+
       token = strtok(buf, " ");
       while(token)
       {
         argv[j] = token;
+        fprintf(stderr, "argv[%d] = %s\n", j, argv[j]);
         token = strtok(NULL, " ");
         j++;
       }
       argv[j] = NULL;
 
+      if(!script && argv[0][0] == '.' && argv[0][1] == '/')
+      {
+        script = 1;
+        scriptpath = argv[0]+2;
+        fds = open(scriptpath, O_RDONLY);
+        /*k = 0;
+        while ((i = read(fds, &c, 1)) > 0)
+        {
+          fprintf(stderr, "chara = %c\n", c);
+          if (c == '\0')
+          {
+            script = 0;
+            break;
+          }
+          if (c == '\n')
+          {
+            buf[k] = '\0';
+            printf("command = %s\n", buf);
+            break;
+          }
+          buf[k] = c;
+          k++;
+        }*/
+        //scriptread(fds, buf, script);
+        //fprintf(stderr, "got to continue\n");
+        continue;
+      }
+
+      if(script)
+      {
+        //fprintf(stderr, "Enter the script interpreter\n");
+        /*k = 0;
+        while ( (i = read(fds, &c, 1)) != 0)
+        {
+          fprintf(stderr, "read = %ld;chara = %c\n",i, c);
+          if (c == '\0')
+          {
+            script = 0;
+            break;
+          }
+          if (c == '\n')
+          {
+            buf[k] = '\0';
+            printf("command = %s\n", buf);
+            break;
+          }
+          buf[k] = c;
+          k++;
+        }*/
+        if (scriptread(fds, buf, script)==0)
+        {
+          script = 0;
+          close(fds);
+        }
+      }
+
       if(!strcmp("exit", argv[0]))
-        _exit(0);
+        _exit(atoi(argv[1]));
 
       if (!strcmp("cd", argv[0]))
       {
