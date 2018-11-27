421969f092d3e183e0bc325734e0c0045d1ddf21
kevinlin6543 <kevin.lin6543@gmail.com>
2018-11-27 13:57:02 -0500

    tested mmap

--- OS_Pset5/OS_Pset5.c
+++ OS_Pset5/OS_Pset5.c
@@ -1,4 +1,4 @@
-#$include <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -6,3 +6,22 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <signal.h>
+
+size_t getFilesize(const char* filename)
+{
+    struct stat st;
+    stat(filename, &st);
+    return st.st_size;
+}
+
+int main(int argc, char const *argv[])
+{
+  for (int i = 3; i <= argc; i++)
+  {
+    size_t filesize = getFilesize(argv[i]);
+    int fd = open(argv[1], O_RDONLY);
+    void* mmappedData = mmap(NULL, filesize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_POPULATE, fd, 0);
+    write(1, mmappedData, filesize)
+    return 0;
+  }
+}
