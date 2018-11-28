f3121b58d8a402ab9d2e69a3cfb06887a9c0c39e
kevinlin6543 <kevin.lin6543@gmail.com>
2018-11-28 16:23:32 -0500

    Added Error Checking

--- OS_Pset5/OS_Pset5.c
+++ OS_Pset5/OS_Pset5.c
@@ -10,29 +10,52 @@
 #include <sys/mman.h>
 
 char *s;
+int fd;
 
-size_t getFileSize(const char* filename)
-{
-    struct stat st;
-    stat(filename, &st);
-    return st.st_size;
-}
 int main(int argc, char const *argv[])
 {
-  char *r = argv[2];
+  if (argc < 4)
+  {
+    fprintf(stderr, "Error: not enough arguements. FORMAT: smear TARGET REPLACEMENT file1 {file2 ...}\n");
+    exit(EXIT_FAILURE);
+  }
+  //char *r = argv[2];
   for (int i = 3; i < argc; i++)
   {
-    size_t filesize = getFileSize(argv[i]);
-    int fd = open(argv[i], O_RDWR);
+    struct stat st;
+    if (stat(argv[i], &st) < 0)
+    {
+      fprintf(stderr, "Error: Unable to get stat for file, %s. Will skip file. %s\n", argv[i], strerror(errno));
+      continue;
+    }
+    size_t filesize = st.st_size;
+    if ((fd = open(argv[i], O_RDWR)) < 0)
+    {
+      fprintf(stderr, "Error: Unable to open file, %s, for read and write. Will skip file. %s\n", argv[i], strerror(errno));
+      continue;
+    }
     void* mmappedData = mmap(NULL, filesize, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_POPULATE, fd, 0);
+    if (mmappedData == MAP_FAILED)
+    {
+      fprintf(stderr, "Error: Unable to mmap file, %s, for read and write. Will skip file. %s\n", argv[i], strerror(errno));
+      continue;
+    }
     s = mmappedData;
     while((s = memmem(s, filesize, argv[1], strlen(argv[1]))) != NULL)
     {
-      memcpy(s, r, strlen(r));
+      memcpy(s, argv[2], strlen(argv[2]));
       s++;
     }
-    munmap(mmappedData, filesize);
-    close(fd);
+    if (munmap(mmappedData, filesize) < 0)
+    {
+      fprintf(stderr, "Error: Unable to munmap file, %s. Will move onto next file %s\n", argv[i], strerror(errno));
+      continue;
+    }
+    if (close(fd) < 0)
+    {
+      fprintf(stderr, "Error: Unable to close file, %s. Will move onto next file. %s\n", argv[i], strerror(errno));
+      continue;
+    }
   }
   return 0;
 }
